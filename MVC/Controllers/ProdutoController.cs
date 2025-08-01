ce34e2179e60843e03e6c0a63ec7bb85e74f016d
Carol Chaves <carolina.amaralchaves@gmail.com>
2025-08-01 17:54:44 -0300

    Padrão MVC

---
+++ MVC/Controllers/ProdutoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Produto> list = new List<Produto>
+            {
+                new Produto { Id = 1, Nome = "Notebook", Preco = 3500 },
+                new Produto { Id = 2, Nome = "Teclado", Preco = 189 }
+            };
+
+            return View(list);
+        }
+    }
+}
